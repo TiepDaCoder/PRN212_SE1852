ee35b08d60c576bd24ee9f67ebba1a90f18bc2b1
Phùng Đức Tiệp <tombin082005@gmail.com>
2025-06-17 08:40:08 +0700

    Bài tập control cơ bản 3 - RadioButtonControl

---
+++ RadioButtonControl/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+
+namespace RadioButtonControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnGui_Click(object sender, RoutedEventArgs e)
+        {
+            string binhChon = "";
+            if (radRatTot.IsChecked == true)
+            {
+                binhChon = radRatTot.Content + "";
+            }
+            else if (radTot.IsChecked == true)
+            {
+                binhChon = radTot.Content + "";
+            }
+            else if (radTamTam.IsChecked == true)
+            {
+                binhChon = radTamTam.Content + "";
+            }
+            else if (radKhongOk.IsChecked == true)
+            {
+                binhChon = radKhongOk.Content + "";
+            }
+
+            string gioiTinh = "";
+            if (radNam.IsChecked == true)
+            {
+                gioiTinh = radNam.Content + "";
+            }
+            else if (radNu.IsChecked == true)
+            {
+                gioiTinh = radNu.Content + "";
+            }
+
+            string infor = "Bạn đã chọn: " + binhChon + Environment.NewLine;
+            infor += "Giới tính: " + gioiTinh;
+            MessageBoxResult ret;
+            ret = MessageBox.Show(infor, "Mời bạn xác nhận", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (ret == MessageBoxResult.Yes)
+            {
+
+            }
+        }
+        private void BtnHuy_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
