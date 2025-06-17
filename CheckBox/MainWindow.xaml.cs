772b4be9f9a86342200365530706d4568b4574e8
Phùng Đức Tiệp <tombin082005@gmail.com>
2025-06-17 08:13:18 +0700

    Bài tập control cơ bản 2 - CheckBox

---
+++ CheckBox/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace CheckBoxControlEvent
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ChkToanBo_Checked(object sender, RoutedEventArgs e)
+        {
+            chkTech.IsChecked = true;
+            chkTour.IsChecked = true;
+            chkFootball.IsChecked = true;
+            chkMovie.IsChecked = true;
+        }
+        private void ChkToanBo_Unchecked(object sender, RoutedEventArgs e)
+        {
+            chkTech.IsChecked = false;
+            chkTour.IsChecked = false;
+            chkFootball.IsChecked = false;
+            chkMovie.IsChecked = false;
+        }
+        private void chkSub_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            if (chkTech.IsChecked == true && chkTour.IsChecked == true && chkFootball.IsChecked == true && chkMovie.IsChecked == true)
+            {
+                chkAll.IsChecked = true;
+            }
+            else if (chkTech.IsChecked == false && chkTour.IsChecked == false && chkFootball.IsChecked == false && chkMovie.IsChecked == false)
+                chkAll.IsChecked = false;
+        }
+    }
+}
